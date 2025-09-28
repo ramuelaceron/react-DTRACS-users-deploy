4f3e298e4a448ab897399443b72cf36d9873a70c
Ramuel Aceron <aceronramuel@gmail.com>
2025-09-28 20:34:01 +0800

    Fix

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@
 const savedApiUrl = localStorage.getItem("API_BASE_URL");
 
 // Default fallback
-const defaultApiUrl = "https://parish-soon-involve-tires.trycloudflare.com";
+const defaultApiUrl = "https://socks-theaters-shaft-kurt.trycloudflare.com";
 
 const config = {
   API_BASE_URL: (savedApiUrl || defaultApiUrl).trim(),
